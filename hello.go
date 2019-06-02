ba5b9c0e2e0268567a1ee7d283323ec9b3e289cf
Atreya <atreya@PC103922.local>
2019-06-02 21:36:08 +0900

    refactor switch to greetingPrefix

--- hello.go
+++ hello.go
@@ -11,16 +11,20 @@ func Hello(name, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case "French":
 		prefix = frenchHelloPrefix
 	case "Spanish":
 		prefix = spanishHelloPrefix
-	}
+	default:
+		prefix = englishHelloPrefix
 
-	return prefix + name
+	}
+	return
 }
 
 func main() {
