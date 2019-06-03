5268d489530f69c0d177c395f8a2db0d6e2bee3b
Atreya <atreya@PC103922.local>
2019-06-03 09:38:26 +0900

    first commit

---
+++ iteration_test.go
@@ -0,0 +1,12 @@
+package iteration
+
+import "testing"
+
+func TestRepeated(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected '%s' but got '%s'", expected, repeated)
+	}
+}
