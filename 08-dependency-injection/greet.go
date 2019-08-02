00b04f0364c4487d05772d21f2fbed19b4f59457
Atreya <atreya2011@gmail.com>
2019-08-02 21:06:54 +0900

    refactor

--- 08-dependency-injection/greet.go
+++ 08-dependency-injection/greet.go
@@ -1,10 +1,15 @@
-package greet
+package main
 
 import (
 	"bytes"
 	"fmt"
+	"os"
 )
 
 func Greet(writer *bytes.Buffer, name string) {
 	fmt.Fprintf(writer, "Hello, %s\n", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
 }
