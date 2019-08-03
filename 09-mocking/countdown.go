66552d49ce6123fd0cada3f63c0ab08f0a033125
Atreya <atreya2011@gmail.com>
2019-08-03 13:31:37 +0900

    add time.Sleep

--- 09-mocking/countdown.go
+++ 09-mocking/countdown.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const (
@@ -17,7 +18,9 @@ func main() {
 
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
+	time.Sleep(1 * time.Second)
 	fmt.Fprint(out, finalWord)
 }
